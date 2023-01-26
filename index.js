bf79bc15906cf0683c4603a62051b2eacbc0d202
awaibl <1910653268@stud.fh-kufstein.ac.at>
2023-01-26 15:55:04 +0100

    socket.io

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server); //socket.io instance
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-  });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected'); //listen to connection
+    socket.on('disconnect', () => {
+        console.log('user disconnected'); //speical disconnect event
+    });
+});
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+}); //send message
+
+
